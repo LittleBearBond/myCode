2ef2d1df99de64c07788c5e96b6b1af97d647468
LittleBearBond <xj_bond@live.com>
2014-12-11 11:50:39 +0800

    study less

---
+++ myTest/test/ajax-page.php
@@ -0,0 +1,15 @@
+<?php
+sleep(1);
+?>
+<div class="page out" data-id="<?php echo $_REQUEST['curr_page']; ?>">
+	<div class="wrap-text">答案没有对错之分，未免影响结果，请真实作答。 </div>
+	<h1><?php echo $_REQUEST['curr_page']; ?>-7. 若你有时间和金钱，你的朋友邀请你 到国外度假，并且在前一天才通知，你会： </h1>
+	<div class="content" data-result="A">A、未来，关于改进活发明事物和生活的种 种可能性。你也许会谈论一个新的科学 发明，或一个更好的方法来表达你的感受。</div>
+	<div class="content" data-result="B">B、实际的、具体的、关于“此时此地” 的事物。例如你或许会谈论品酒的方法，或 你即将要参加新奇旅程。</div>
+	<a href="javascript:" class="btn-pre">重答上一题</a>
+	<div class="wrap-imgs">
+		<img src="http://5.f1.dajieimg.com/n/micro_blog/T10RDvBbhT1R4cSCrK_c.png" alt="">
+		<i></i>
+		<img src="http://5.f1.dajieimg.com/n/micro_blog/T1atDvBTbT1R4cSCrK_c.png" alt="">
+	</div>
+</div>
