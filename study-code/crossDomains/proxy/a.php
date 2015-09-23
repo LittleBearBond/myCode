53093d1231229bcb385d479c66621d9c715ab5bf
xiongjian <xj_bond@live.com>
2015-09-23 10:51:14 +0800

    add file crossDomain

---
+++ study-code/crossDomains/proxy/a.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+    <meta content="telephone=no" name="format-detection">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta name="full-screen" content="true">
+    <meta name="screen-orientation" content="portrait">
+    <meta name="x5-fullscreen" content="true">
+    <meta name="360-fullscreen" content="true">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <style type="text/css">
+        body{
+            background-color: #ccc;
+        }
+        p{
+            font-size: 30px;
+            color: #fff;
+        }
+    </style>
+</head>
+<body>
+    <p>aaaaaaa</p>
+
+    <script src="http://libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="./cross-domains-proxy.js"></script>
+    <script type="text/javascript">
+
+        <?php
+        $js = $_REQUEST['js'];
+        echo $js;
+        ?>
+        console.log(top.window.$)
+    </script>
+</body>
+</html>
