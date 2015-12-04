d1e204b68e249906022bb6bb073bdbfb8c9059d1
xiongjian <xj_bond@live.com>
2015-12-04 14:19:04 +0800

    modify

--- study-code/shell/sh/awk.sh
+++ study-code/shell/sh/awk.sh
@@ -4,13 +4,4 @@ ls -al | awk '{print $5,$9}'| sort -nr
 du -s * | sort -nr | head -1
 
 
-# 统计文件大小
-#sed -n '$=' filename
-#grep '.*' -c filename
-#perl -ne 'END {print "$i"} ++$i' filename
-#perl -ne 'END {print $.."\n"}' filename
-#awk 'END{print NR}' filename
-#awk '{print NR}' filename |tail -n1
-#grep -n "" filename|awk -F: '{print '}|tail -n1
-#wc -l filename|awk '{print }'
-#cat filename |wc -l
+
