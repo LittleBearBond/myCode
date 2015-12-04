5095820f52c3a721265bc5e176832a460f07a146
xiongjian <xj_bond@live.com>
2015-12-04 11:22:27 +0800

    重构

--- study-code/nodejs/getFilesByDir/test-main.js
+++ study-code/nodejs/getFilesByDir/test-main.js
@@ -5,14 +5,21 @@ var path = require("path");
 var getFilesByDir = require("./getFilesByDir-1.js");
 var getMaxFile = require("./1-4.js");
 var getfiles = require("./getFilesByDir-2.js");
-var getallfiles = require("./getFilesByDir-3.es6");
 
-var readPth = path.join(__dirname, "../");
+
+var readPth = 'E:/Work-XDF/code/static/project/cms-mix/1.x/js'; // path.join(__dirname, "../");
+var topNum = 10;
 var results = [];
 var logResult = function(err, result) {
+    if (!result) {
+        result = err;
+    }
     Array.prototype.push.apply(results, result);
 
-    results.sort(function(pre, next) {
+    results = results.filter(function(curr) {
+        return curr.fullPath && curr.fullPath.trim().endsWith('.js');
+    });
+    results = results.sort(function(pre, next) {
         if (next.size > pre.size) {
             return 1;
         }
@@ -22,23 +29,15 @@ var logResult = function(err, result) {
         return 0;
     });
 
+    results = results.slice(0, topNum);
+
     console.log(results.map(function(curr) {
-        return curr.size + "----------" + curr.fullPath;
+        return curr.size + ":" + curr.fullPath;
     }).join("\n"));
 };
 
 /*getFilesByDir(readPth, logResult);
-
-getfiles(readPth).then(function(result) {
+ */
+getfiles(readPth).then(logResult, function(result) {
     console.log(result);
-}, function(result) {
-    console.log(result);
-});*/
-
-getallfiles(readPth);
-
-/*getMaxFile('./')
-    .then(function(filename) {
-        console.log('largest file is:', filename);
-    });
-*/
+});
