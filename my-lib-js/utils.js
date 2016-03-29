70ab58d4b7df88d5b5ddad86668cf1c6deacc9da
xiongjian <xiongjian@didichuxing.com>
2016-03-29 11:00:47 +0800

    modify

--- my-lib-js/utils.js
+++ my-lib-js/utils.js
@@ -1,3 +1,14 @@
+/**
+ * @param {String}  errorMessage   错误信息
+ * @param {String}  scriptURI      出错的文件
+ * @param {Long}    lineNumber     出错代码的行号
+ * @param {Long}    columnNumber   出错代码的列号
+ * @param {Object}  errorObj       错误的详细信息，Anything
+ */
+window.onerror = function(errorMessage, scriptURI, lineNumber, columnNumber, errorObj) {
+	// code..
+	console.log(errorMessage)
+}
 window.onerror = function(msg, url, line, col, error) {
 	//没有URL不上报！上报也不知道错误
 	if (msg != "Script error." && !url) {
