ed0e0427bc287b1342ca037939a0219a874a2ac3
xiongjian <xiongjian@didichuxing.com>
2017-05-27 16:17:13 +0800

    add LeetCode 165. Compare Version Numbers

--- some-js/LeetCode.js
+++ some-js/LeetCode.js
@@ -285,3 +285,24 @@ var isPalindrome = function (str) {
 }
 console.log(isPalindrome("A man, a plan, a canal: Panama"))
 console.log(isPalindrome("sdfd df"))
+
+// LeetCode 165. Compare Version Numbers
+var compareVersion = function (version1, version2) {
+    var v1 = version1.split('.').map(val => val | 0)
+    var v2 = version2.split('.').map(val => val | 0)
+    var len1 = v1.length;
+    var len2 = v2.length;
+    var len = len1 < len2 ? len1 : len2;
+    var index = 0;
+    for (; index < len; index++) {
+        if (v1[index] === v2[index]) {
+            continue;
+        }
+        return v1[index] > v2[index] ? 1 : -1;
+    }
+    return 0;
+}
+console.log(compareVersion('1.1.5', '1.2.2'))
+console.log(compareVersion('0.1.5', '1.2.2'))
+console.log(compareVersion('1.3.5', '1.2.2'))
+console.log(compareVersion('1.1.1', '1.1.1'))
