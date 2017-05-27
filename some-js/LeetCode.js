7ed13a469ad228e11d91e469e2208e40f2d97596
xiongjian <xiongjian@didichuxing.com>
2017-05-27 16:20:52 +0800

    fix logic

--- some-js/LeetCode.js
+++ some-js/LeetCode.js
@@ -288,17 +288,20 @@ console.log(isPalindrome("sdfd df"))
 
 // LeetCode 165. Compare Version Numbers
 var compareVersion = function (version1, version2) {
-    var v1 = version1.split('.').map(val => val | 0)
-    var v2 = version2.split('.').map(val => val | 0)
+    var v1 = version1.split('.')
+    var v2 = version2.split('.')
     var len1 = v1.length;
     var len2 = v2.length;
-    var len = len1 < len2 ? len1 : len2;
-    var index = 0;
+    var len = len1 > len2 ? len1 : len2;
+    var index = 0,
+        i1, i2;
     for (; index < len; index++) {
-        if (v1[index] === v2[index]) {
+        i1 = v1[index] | 0;
+        i2 = v2[index] | 0;
+        if (i1 === i2) {
             continue;
         }
-        return v1[index] > v2[index] ? 1 : -1;
+        return i1 > i2 ? 1 : -1;
     }
     return 0;
 }
@@ -306,3 +309,4 @@ console.log(compareVersion('1.1.5', '1.2.2'))
 console.log(compareVersion('0.1.5', '1.2.2'))
 console.log(compareVersion('1.3.5', '1.2.2'))
 console.log(compareVersion('1.1.1', '1.1.1'))
+console.log(compareVersion('1.1.1', '1.1.1.0.1'))
