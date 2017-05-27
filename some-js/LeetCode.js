0a466df4614753f726431d35438ce87b42b4efa6
xiongjian <xiongjian@didichuxing.com>
2017-05-27 14:56:47 +0800

    update

--- some-js/LeetCode.js
+++ some-js/LeetCode.js
@@ -138,3 +138,26 @@ var isValid = function (str) {
     }
     return stack.length === 0;
 }
+// LeetCode 58. Length of Last Word
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+    if (!s) {
+        return 0;
+    }
+    var arr = s.split(/\s+/);
+    if (!arr.length) {
+        return 0;
+    }
+    var str = arr.pop();
+    while (arr.length) {
+        if (str.length) {
+            return str.length;
+        }
+        str = arr.pop();
+    }
+    return 0;
+}
+// LeetCode 14. Longest Common Prefix
