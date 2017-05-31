a4cf804ce82cb81f79f19cd5386618063b7fed8a
xiongjian <xiongjian@didichuxing.com>
2017-05-31 14:44:21 +0800

    add LeetCode 349. Intersection of Two Arrays

--- some-js/LeetCode.js
+++ some-js/LeetCode.js
@@ -353,3 +353,24 @@ var moveZeroes = function (nums) {
 
 console.log(moveZeroes([1, 2, 3, 0, 4, 0, 5]))
 console.log(moveZeroes([1, 2, 3, 0, 4, 0, 5, 0]))
+
+// LeetCode 349. Intersection of Two Arrays
+var intersection = function (nums1, nums2) {
+    var len1 = nums1.length;
+    var len2 = nums2.length;
+    var map = {},
+        arr = [];
+    nums1.forEach(val => {
+        if (!(val in map)) {
+            map[val] = true;
+        }
+    })
+    for (let item of new Set(nums2)) {
+        if (item in map) {
+            arr.push(item)
+        }
+    }
+    return arr;
+}
+console.log(intersection([1, 2, 2, 1], [2, 2]))
+console.log(intersection([1, 2, 2, 1, 3, 4, 5], [2, 2, 3, 3]))
