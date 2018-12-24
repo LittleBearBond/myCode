3887726ebd9e64745fc7f175cae0dd5e10c6249b
xiongjian <xiongjian@didichuxing.com>
2018-12-24 12:14:59 +0800

    throtte-debounce

---
+++ some-js/throtte-debounce.js
@@ -0,0 +1,17 @@
+/**
+ * 函数节流
+ * @param {*} fn
+ * @param {*} time 每隔time执行一次
+ */
+export function throtte(fn, time) {
+
+}
+
+/**
+ * 函数放抖动
+ * @param {*} fn
+ * @param {*} time 每次延迟time执行一次
+ */
+export function debounce(fn, time) {
+
+}
