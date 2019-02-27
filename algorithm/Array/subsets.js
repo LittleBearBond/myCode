af6700be74f4062f7f558051398545fd42b0dc29
xiongjian <xiongjian@didichuxing.com>
2019-02-27 16:22:36 +0800

    add subsets

--- algorithm/Array/subsets.js
+++ algorithm/Array/subsets.js
@@ -6,5 +6,13 @@
  */
 var subsets = function (nums) {
 	var result = []
-
+	for (const n of nums) {
+		for (var i = 0, len = result.length; i < len; i++) {
+			result.push([...result[i], n])
+		}
+		result.push([n])
+	}
+	result.push([])
+	return result
 };
+console.log(subsets([1, 2, 3]))
