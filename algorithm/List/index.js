dcf1cdfeafdc2fea3b7402703d07780f568f626d
xiongjian <xiongjian@didichuxing.com>
2019-03-15 16:13:25 +0800

    add link list

---
+++ algorithm/List/index.js
@@ -0,0 +1,43 @@
+function Node(val) {
+    this.val = val
+    this.next = null
+}
+
+function List(array) {
+    this.head = null
+    let i = 0,
+        temp = null
+    while (i < array.length) {
+        if (i === 0) {
+            this.head = new Node(array[i])
+            temp = this.head
+        } else {
+            let newNode = new Node(array[i])
+            temp.next = newNode
+            temp = temp.next
+        }
+        i++
+    }
+}
+
+function traverseList(listHead) {
+    while (listHead) {
+        console.log(listHead.val)
+        listHead = listHead.next
+    }
+}
+
+function reverseList(listHead) {
+    let pre = null
+    while (listHead) {
+        // 拿到next
+        let next = listHead.next
+        // 把当前节点的指到上一个节点
+        listHead.next = pre
+        //下一个节点的前一个节点就是当前节点
+        pre = listHead
+        // 移动到下一个节点
+        listHead = next
+    }
+    return pre
+}
