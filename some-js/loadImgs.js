49986b39402ce0a18f7556600169e7d34bcd702c
xiongjian <xiongjian@didichuxing.com>
2019-03-20 14:15:55 +0800

    udpate loadImgs

--- some-js/loadImgs.js
+++ some-js/loadImgs.js
@@ -1,26 +1,26 @@
 var urls = [
-  "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2168427908,4072089613&fm=200&gp=0.jpg",
-  "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=281396915,1736731169&fm=26&gp=0.jpg",
-  "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=356729896,1181030408&fm=200&gp=0.jpg",
-  "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2670795854,3778606258&fm=26&gp=0.jpg",
-  "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=4135477902,3355939884&fm=11&gp=0.jpg",
-  "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2715201274,348455391&fm=200&gp=0.jpg",
-  "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1677996155,3201691996&fm=26&gp=0.jpg",
-  "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=305514179,563565534&fm=26&gp=0.jpg",
-  "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=436693990,4197026682&fm=26&gp=0.jpg",
-  "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1778162608,400941225&fm=26&gp=0.jpg",
-  "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=466110605,1135815309&fm=200&gp=0.jpg",
-  "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1836611714,1036444481&fm=200&gp=0.jpg",
-  "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=142902671,758482349&fm=200&gp=0.jpg",
-  "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1627114934,1859383776&fm=200&gp=0.jpg",
-  "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2784092765,886171263&fm=200&gp=0.jpg",
-  "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2540594264,2400204506&fm=26&gp=0.jpg",
-  "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=562672488,2642012468&fm=200&gp=0.jpg",
-  "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=233716865,3298473498&fm=200&gp=0.jpg",
-  "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=169085166,1650057873&fm=200&gp=0.jpg",
-  "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1160628485,2996191523&fm=200&gp=0.jpg",
-  "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3704910016,3579983578&fm=200&gp=0.jpg",
-  "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2585817365,4270330179&fm=200&gp=0.jpg"
+    "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2168427908,4072089613&fm=200&gp=0.jpg",
+    "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=281396915,1736731169&fm=26&gp=0.jpg",
+    "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=356729896,1181030408&fm=200&gp=0.jpg",
+    "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2670795854,3778606258&fm=26&gp=0.jpg",
+    "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=4135477902,3355939884&fm=11&gp=0.jpg",
+    "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2715201274,348455391&fm=200&gp=0.jpg",
+    "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1677996155,3201691996&fm=26&gp=0.jpg",
+    "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=305514179,563565534&fm=26&gp=0.jpg",
+    "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=436693990,4197026682&fm=26&gp=0.jpg",
+    "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1778162608,400941225&fm=26&gp=0.jpg",
+    "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=466110605,1135815309&fm=200&gp=0.jpg",
+    "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1836611714,1036444481&fm=200&gp=0.jpg",
+    "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=142902671,758482349&fm=200&gp=0.jpg",
+    "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1627114934,1859383776&fm=200&gp=0.jpg",
+    "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2784092765,886171263&fm=200&gp=0.jpg",
+    "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2540594264,2400204506&fm=26&gp=0.jpg",
+    "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=562672488,2642012468&fm=200&gp=0.jpg",
+    "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=233716865,3298473498&fm=200&gp=0.jpg",
+    "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=169085166,1650057873&fm=200&gp=0.jpg",
+    "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1160628485,2996191523&fm=200&gp=0.jpg",
+    "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3704910016,3579983578&fm=200&gp=0.jpg",
+    "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2585817365,4270330179&fm=200&gp=0.jpg"
 ]
 
 function loadImg(url) {
@@ -62,6 +62,24 @@ function limitLoad(urls, handler, limit) {
         .then(() => Promise.all(promises))
 }
 limitLoad(urls, loadImg, 3)
+
+function loadImgs(urls, n = 3) {
+    const preUrl = urls.slice(0, n)
+    const nextUls = urls.slice(n)
+    const promise = preUrl.map((url, index) => loadImg(url).then(() => index));
+    let i = 0
+    let {
+        length
+    } = nextUls
+    const load = index => {
+        if (i < length) {
+            promise[index] = loadImg(nextUls[i++]).then(() => index, () => index)
+            Promise.race(promise).then(load)
+        }
+    }
+    Promise.race(promise).then(load)
+    Promise.all(promise).then(() => console.log('all loaded'))
+}
 
 /*
 因为 limitLoad 函数也返回一个 Promise，所以当 所有图片加载完成后，可以继续链式调用
